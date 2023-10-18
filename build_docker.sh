6c4086854b11e8b5582324c7eaef186cc82635c4
Yuan Jian Puah <pyuanjian@google.com>
2023-10-18 16:52:01 +0800

    added parameters for running docker image in cloud shell

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,6 @@
 
 # Reference: https://medium.com/@faizififita1/how-to-deploy-your-streamlit-web-app-to-google-cloud-run-ba776487c5fe
 image_name="streamlit-app"
-docker build -t ${image_name} .
+docker_config="CloudShellDockerfile"
+
+docker build -f ${docker_config} -t ${image_name} . 
