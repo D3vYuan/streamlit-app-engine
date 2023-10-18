ea688d8fc914403df43329ebdbf7fe71701864ca
Yuan Jian Puah <pyuanjian@google.com>
2023-10-18 16:48:10 +0800

    updated destination port for run script due to change in Dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,8 @@
 
 image_name="streamlit-app"
 source_port=8080
-destination_port=8501
+#destination_port=8501
+destination_port=8080
 
 echo "docker run -itd --rm -p ${source_port}:${destination_port} ${image_name}"
 docker run -itd --rm -p ${source_port}:${destination_port} ${image_name}
