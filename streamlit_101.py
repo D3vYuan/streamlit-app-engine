092adb9bc9561bd857dbd6c2a285a55bd902f976
Yuan Jian Puah <pyuanjian@google.com>
2023-10-18 09:42:07 +0800

    created simple streamlit app

--- streamlit_101.py
+++ streamlit_101.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.write("Hello World")
+
+def handle_btn_click():
+    if "btn_hello" not in st.session_state:
+        st.session_state.btn_hello = 0
+    st.session_state.btn_hello += 1
+    st.write(f"Hello World x {st.session_state.btn_hello}")
+
+
+def clear_btn_click():
+    if "btn_hello" in st.session_state:
+        st.session_state.btn_hello = 0
+    st.write(f"Hello World x {st.session_state.btn_hello}")
+    # st.write("Cleared")
+
+
+st.title("Hello World")
+col1, col2 = st.columns(2)
+with col1:
+    st.button(label="Hello World", on_click=handle_btn_click)
+with col2:
+    st.button(label="Reset", on_click=clear_btn_click)
